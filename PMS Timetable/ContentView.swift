c0f22b69a833ff70ae29a47a659943b45d574a75
Adon Omeri <adon.omeri@student.education.wa.edu.au>
2026-04-25 13:54:42 +0800

    Initial Commit

---
+++ PMS Timetable/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PMS Timetable
+//
+//  Created by Adon Omeri on 25/4/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PMS Timetable/PMS_TimetableApp.swift
@@ -0,0 +1,17 @@
+//
+//  PMS_TimetableApp.swift
+//  PMS Timetable
+//
+//  Created by Adon Omeri on 25/4/2026.
+//
+
+import SwiftUI
+
+@main
+struct PMS_TimetableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
