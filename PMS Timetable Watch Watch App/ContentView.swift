a57d7de7168069dfe5482cbb9873938ea271a0e0
Adon Omeri <adon.omeri@student.education.wa.edu.au>
2026-04-26 11:15:09 +0800

    elrgbhegglii

---
+++ PMS Timetable Watch Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PMS Timetable Watch Watch App
+//
+//  Created by Adon Omeri on 26/4/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
